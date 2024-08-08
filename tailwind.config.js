34f4daed27392a8ee5bc902bb2c7c937db23749d
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-08-08 23:11:05 +0700

    feat: add container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			container: {
+				center: true,
+				padding: '2rem'
+			}
+		}
 	},
 	plugins: []
 };
