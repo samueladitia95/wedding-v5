e2840fb58c6bb9d03ae268d72725da5a1acbdabb
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-08-08 20:13:33 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	// const state1: string = 'state1'; // ? state
+	// let prop1: string; // ? required props
+	// let prop2: string = 'prop2'; // ? props with default value
+	// $: computed1 = state1 * 2 // ? computed value
+</script>
+
+<h1 class="text-3xl font-bold underline text-blue-600">Hello world!</h1>
