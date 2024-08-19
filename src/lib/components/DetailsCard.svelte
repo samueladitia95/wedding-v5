0ebfe8e03a61f086af6032697d707f3a7b382a5f
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-08-19 21:43:16 +0700

    update

--- src/lib/components/DetailsCard.svelte
+++ src/lib/components/DetailsCard.svelte
@@ -3,14 +3,14 @@
 	export let name: string;
 	export let location: string;
 	export let time: string;
-	export let condition: string;
-	export let condition2: string;
+	export let condition: string | undefined;
+	export let condition2: string | undefined;
 	export let image: any;
 	export let isLast: boolean;
 </script>
 
 <div class="flex flex-col items-center justify-center font-gordita">
-	<img src={image} />
+	<img src={image} alt="satu" />
 	<p class="pt-8 pb-2 tracking-wider">{name}</p>
 	{#if condition}
 		<p class="text-sm font-timesNewRoman">{condition}</p>
@@ -20,7 +20,7 @@
 	{/if}
 	<p class="py-6 tracking-wider">{time}</p>
 	<p class="text-xs">{location}</p>
-    {#if !isLast}
-	<img src={divider} class="mt-16"/>
-    {/if}
+	{#if !isLast}
+		<img src={divider} class="mt-16" alt="dua" />
+	{/if}
 </div>
