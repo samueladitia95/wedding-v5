f2f9167da179c7dd3971bc423c446fa3e068d799
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-08-20 03:58:58 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import LetterOpener from './containers/letterOpener.svelte';
 	import Rsvp from './containers/Rsvp.svelte';
 	import Attire from './containers/Attire.svelte';
 	import WeddingGift from './containers/WeddingGift.svelte';
@@ -19,9 +18,8 @@
 	// let prop2: string = 'prop2'; // ? props with default value
 	// $: computed1 = state1 * 2 // ? computed value
 
-	let isInvitationOpened = true;
+	let isInvitationOpened = false;
 	const openInvitation = () => {
-		console.log('masuk kok');
 		isInvitationOpened = true;
 	};
 	export let data: PageData;
@@ -31,37 +29,33 @@
 	});
 </script>
 
-{#if !isInvitationOpened}
-	<LetterOpener {openInvitation} />
-{/if}
-{#if isInvitationOpened}
-	<Intro {data} />
-	<Started {data} />
-	<TogetherWith {data} />
-	<SaveTheDate />
-	<Map {data} />
-	<Details />
-	<TravelGuide {data} />
-	<Attire {data} />
-	<QnA {data} />
-	<div class="lg:hidden">
-		<Rsvp />
-		<Wishes />
-	</div>
-	<div class="bg-mj-sand hidden lg:flex lg:flex-row justify-center w-full">
-		<div class="grid grid-cols-2 w-full xl:min-w-[1440px]">
-			<div class="bg-mj-sand2 flex flex-row justify-end pl-8">
-				<Rsvp />
-			</div>
-			<div class="bg-mj-dark-sand flex justify-center pr-8">
-				<Wishes />
-			</div>
+<Intro {data} {isInvitationOpened} {openInvitation} />
+<Started {data} />
+<TogetherWith {data} />
+<SaveTheDate />
+<Map {data} />
+<Details />
+<TravelGuide {data} />
+<Attire {data} />
+<QnA {data} />
+<div class="lg:hidden">
+	<Rsvp />
+	<Wishes />
+</div>
+<div class="bg-mj-sand hidden lg:flex lg:flex-row justify-center w-full">
+	<div class="grid grid-cols-2 w-full xl:min-w-[1440px]">
+		<div class="bg-mj-sand2 flex flex-row justify-end pl-8">
+			<Rsvp />
+		</div>
+		<div class="bg-mj-dark-sand flex justify-center pr-8">
+			<Wishes />
 		</div>
 	</div>
-	<WeddingGift {data} />
-	<div
-		class="px-[24px] py-[16px] flex text-center justify-center bg-[#594F47] text-white text-[12px]"
-	>
-		<p>Website Invitation by Another Chapter</p>
-	</div>
-{/if}
+</div>
+<WeddingGift {data} />
+<div
+	class="px-[24px] py-[16px] flex text-center justify-center bg-[#594F47] text-white text-[12px]"
+>
+	<p>Website Invitation by Another Chapter</p>
+</div>
+<!-- {/if} -->
--- src/routes/containers/Intro.svelte
+++ src/routes/containers/Intro.svelte
@@ -3,65 +3,89 @@
 	import { pb } from '$lib/pocketbase';
 	export let data: PageData;
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import PrimaryButton from '$lib/components/PrimaryButton.svelte';
 
 	let isLoading = true;
-	onMount(() => {
-		setTimeout(() => {
-			isLoading = false;
-		}, 1200); // 2 seconds delay
-	});
+	export let isInvitationOpened: boolean;
+
+	$: {
+		if (isInvitationOpened) {
+			setTimeout(() => {
+				isLoading = false;
+			}, 4000); // 2 seconds delay
+		}
+	}
 	const introLoadingText1 = 'The Wedding Of';
 	const introLoadingText2 = 'Marco & Jessica';
 
 	const introMainText =
 		'Distance is not for the fearful, it is for the bold. It’s for those who are willing to spend a lot time  alone in exchange for a little time with the one they love.';
+
+	import logo from '$lib/assets/MJ Logo.svg';
+	export let openInvitation: () => void;
 </script>
 
-<div class="bg-mj-black min-h-screen flex justify-center relative">
-	<div class="min-h-screen flex justify-center relative">
-		<img
-			class="absolute top-0 left-0 md:hidden h-screen w-screen object-cover"
-			src={pb.files.getUrl(data.main, data.main.main_image_mobile)}
-			alt="1"
-		/>
-		<img
-			class="h-screen w-screen absolute top-0 left-0 hidden md:block lg:hidden object-cover"
-			src={pb.files.getUrl(data.main, data.main.main_image_tablet)}
-			alt="tablet-img"
-		/>
-		<img
-			class="h-screen w-screen absolute top-0 left-0 hidden lg:block object-cover"
-			src={pb.files.getUrl(data.main, data.main.main_image_desktop)}
-			alt="desktop-img"
-		/>
-		<div
-			class="z-20 w-screen flex flex-col justify-between py-[72px] px-[62px] md:px-44 xl:px-96 font-aboreto leading-loose"
-		>
-			<p class="text-white text-[16px] md:text-[24px] lg:text-[20px] text-center">
-				{#if !isLoading}
-					#sayJESStoMARCO
-				{/if}
-			</p>
-			<div
-				class="text-white text-base/relaxed tracking-widest md:text-xl/relaxed lg:text-lg/relaxed text-center"
-			>
-				{#if isLoading}
-					<p class="pb-">
-						{introLoadingText1}
+<div class="bg-mj-beige min-h-screen">
+	{#if isInvitationOpened}
+		<div class="flex justify-center relative" in:fade={{ duration: 2000, delay: 2000 }}>
+			<div class="min-h-screen flex justify-center relative">
+				<img
+					class="absolute top-0 left-0 md:hidden h-screen w-screen object-cover"
+					src={pb.files.getUrl(data.main, data.main.main_image_mobile)}
+					alt="1"
+				/>
+				<img
+					class="h-screen w-screen absolute top-0 left-0 hidden md:block lg:hidden object-cover"
+					src={pb.files.getUrl(data.main, data.main.main_image_tablet)}
+					alt="tablet-img"
+				/>
+				<img
+					class="h-screen w-screen absolute top-0 left-0 hidden lg:block object-cover"
+					src={pb.files.getUrl(data.main, data.main.main_image_desktop)}
+					alt="desktop-img"
+				/>
+				<div
+					class="z-20 w-screen flex flex-col justify-between py-[72px] px-[62px] md:px-44 xl:px-96 font-aboreto leading-loose"
+				>
+					<p class="text-white text-[16px] md:text-[24px] lg:text-[20px] text-center">
+						{#if !isLoading}
+							#sayJESStoMARCO
+						{/if}
 					</p>
-					<p>
-						{introLoadingText2}
-					</p>
-				{/if}
-				{#if !isLoading}
-					<div class="font-ivora">
-						{introMainText}
+					<div
+						class="text-white text-base/relaxed tracking-widest md:text-xl/relaxed lg:text-lg/relaxed text-center"
+					>
+						{#if isLoading}
+							<p class="pb-">
+								{introLoadingText1}
+							</p>
+							<p>
+								{introLoadingText2}
+							</p>
+						{/if}
+						{#if !isLoading}
+							<div class="font-ivora">
+								{introMainText}
+							</div>
+						{/if}
 					</div>
-				{/if}
-			</div>
-			<div class="relative">
-				<div class="absolute -left-10 md:-left-32 xl:-left-80 -top-2" />
+					<div class="relative">
+						<div class="absolute -left-10 md:-left-32 xl:-left-80 -top-2" />
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
+	{:else}
+		<div
+			class="min-h-screen flex flex-col justify-evenly items-center w-full"
+			out:fade={{ duration: 2000 }}
+		>
+			<div></div>
+			<img src={logo} alt="logo" />
+			<button on:click={openInvitation}>
+				<PrimaryButton buttonText={'open invitation'} />
+			</button>
+		</div>
+	{/if}
 </div>
