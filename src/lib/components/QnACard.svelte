15043f6268ef0c77fae798040e2a3ef471979f93
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-08-20 02:12:39 +0700

    update

--- src/lib/components/QnACard.svelte
+++ src/lib/components/QnACard.svelte
@@ -10,35 +10,47 @@
 	export let index: number;
 </script>
 
-<div class={`flex flex-row gap-4 ${isLast? '':'border-b'} border-b-white/20 ${selected? 'pb-14': 'pb-2' }`}>
-	<p class="tracking-widest">0{index+1}.</p>
+<div
+	class={`flex flex-row gap-4 ${isLast ? '' : 'border-b'} border-b-white/20 ${selected ? 'pb-14' : 'pb-2'}`}
+>
+	<p class="tracking-widest">0{index + 1}.</p>
 	<div class="text-xs leading-6 w-full">
 		<div class="flex justify-between">
 			<div>
-				<button class="pb-4 text-base hover:cursor-pointer tracking-[0.2em] lg:text-lg" on:click={()=>{selected=!selected}}>
+				<button
+					class="pb-4 text-base hover:cursor-pointer tracking-[0.2em] lg:text-lg"
+					on:click={() => {
+						selected = !selected;
+					}}
+				>
 					{question}
 				</button>
 			</div>
 			<div>
-				<button class="hover:cursor-pointer" on:click={()=>{selected=!selected}}>
-					<div class={`${selected?'rotate-0':'rotate-180'}`}>{@html arrowDown}</div>
+				<button
+					class="hover:cursor-pointer"
+					on:click={() => {
+						selected = !selected;
+					}}
+				>
+					<div class={`${selected ? 'rotate-0' : 'rotate-180'}`}>{@html arrowDown}</div>
 				</button>
 			</div>
 		</div>
-        {#if selected}
-		<div class="tracking-wider text-sm md:text-sm lg:text-base">
-			<p>
-				{answer}
-			</p>
-            {#if conditional}
-			<p class="py-6 font-timesNewRoman italic text-sm md:text-sm lg:text-base">
-                {conditional}
-			</p>
-            {/if}
-            {#if conditional2}
-			<p>{conditional2}</p>
-            {/if}
-		</div>
-        {/if}
+		{#if selected}
+			<div class="tracking-wider text-sm md:text-sm lg:text-base">
+				<p>
+					{answer}
+				</p>
+				{#if conditional}
+					<p class="py-6 font-timesNewRoman italic text-sm md:text-sm lg:text-base">
+						{conditional}
+					</p>
+				{/if}
+				{#if conditional2}
+					<p>{conditional2}</p>
+				{/if}
+			</div>
+		{/if}
 	</div>
 </div>
