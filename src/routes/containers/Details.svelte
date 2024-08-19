ab1101a7ab78d12791bd420a4826f66f451b2f48
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-08-20 03:01:09 +0700

    update

--- src/routes/containers/Details.svelte
+++ src/routes/containers/Details.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { inview, type ObserverEventDetails } from 'svelte-inview';
+	import { fade, fly } from 'svelte/transition';
+
 	const title = 'THE DETAILS';
 
 	import teaPot from '$lib/assets/icons/Teapai Icon.svg';
@@ -37,22 +40,45 @@
 			location: 'THE AMPHITHEATRE, MAYA UBUD'
 		}
 	];
+
+	let isShow: boolean = false;
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>): void => {
+		if (!isShow && detail.inView) isShow = true;
+	};
 </script>
 
-<div class="bg-mj-dark-brown min-h-screen flex justify-center py-28">
+<div
+	class="bg-mj-dark-brown min-h-screen flex justify-center py-28"
+	use:inview={{
+		rootMargin: '-100px',
+		unobserveOnEnter: true
+	}}
+	on:inview_change={handleChange}
+>
 	<div class="px-6 flex flex-col gap-10 justify-center items-center text-white container">
-		<p class="text-3xl md:text-[40px] tracking-widest font-ivora font-light">{title}</p>
+		{#if isShow}
+			<p
+				in:fade={{ duration: 2000 }}
+				class="text-3xl md:text-[40px] tracking-widest font-ivora font-light"
+			>
+				{title}
+			</p>
+		{/if}
 		<div class="flex flex-col gap-16 w-full items-center">
 			{#each items as item, index}
-				<DetailsCard
-					name={item.name}
-					condition={item.condition}
-					condition2={item.condition2}
-					time={item.time}
-					location={item.location}
-					image={item.image}
-					isLast={index === items.length - 1}
-				/>
+				{#if isShow}
+					<div transition:fly={{ x: -400, duration: 1000, delay: 1000 + 500 * (index + 1) }}>
+						<DetailsCard
+							name={item.name}
+							condition={item.condition}
+							condition2={item.condition2}
+							time={item.time}
+							location={item.location}
+							image={item.image}
+							isLast={index === items.length - 1}
+						/>
+					</div>
+				{/if}
 			{/each}
 		</div>
 	</div>
